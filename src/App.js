b8ab28d5c14ca7a9467dfdd2189bdd38d9d4dbe1
Saiki Issei <issei.saiki@pc-2259.brainpad.co.jp>
2025-02-28 16:52:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './Components/SubmitButton'
+import SubmitButton from './Components/SubmitButton';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SubmitButton></SubmitButton>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SubmitButton.js
@@ -0,0 +1,5 @@
+export default function SubmitButton() {
+    return (
+        <button>提出ボタン</button>
+    );
+}
